43a62044422abf3f74ac401267b9d643c4649731
damxaq <damiancelico@gmail.com>
2021-05-11 14:52:02 +0200

    change useState to useRef

--- src/Location.js
+++ src/Location.js
@@ -1,22 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Weather from "./Weather";
 
 const Location = () => {
+  const locationValue = useRef("Poznan");
   const [location, setLocation] = useState("Poznan");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setLocation(locationValue.current.value);
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input
-          name="city"
-          type="text"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-        />
+        <input name="city" type="text" ref={locationValue} />
       </form>
       <Weather location={location} />
     </div>
