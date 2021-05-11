8e66f1632b0cedcdbc3a4ec3a2dccf098ff6b81e
damxaq <damiancelico@gmail.com>
2021-05-11 03:56:48 +0200

    adding fetching coordinates

--- src/Location.js
+++ src/Location.js
@@ -3,24 +3,9 @@ import Weather from "./Weather";
 
 const Location = () => {
   const [location, setLocation] = useState("Poznan");
-  const [weather, setWeather] = useState(undefined);
-  const [loading, setLoading] = useState(false);
-
-  const fetchWeather = async (city) => {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=";
-    const API_KEY = "&appid=59ca3e783384be0ae597d7ff9cef0f84";
-    const response = await fetch(`${url}${city}&units=metric${API_KEY}`);
-    const data = await response.json();
-    console.log(data);
-    setLoading(false);
-    setWeather(data);
-  };
 
   const handleSubmit = (e) => {
-    setLoading(true);
     e.preventDefault();
-    fetchWeather(location);
-    setLocation("");
   };
 
   return (
@@ -33,7 +18,7 @@ const Location = () => {
           onChange={(e) => setLocation(e.target.value)}
         />
       </form>
-      {loading ? <div>Loading...</div> : weather && <Weather {...weather} />}
+      <Weather location={location} />
     </div>
   );
 };
--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Weather = ({ weather, main, wind, visibility }) => {
-  const { temp_min, temp_max } = main;
-  console.log("main", main);
-  return (
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+const LOCATION_URL = "https://api.openweathermap.org/data/2.5/weather?q=";
+const WEATHER_URL = "https://api.openweathermap.org/data/2.5/onecall?";
+
+const Weather = ({ location }) => {
+  const [loading, setLoading] = useState(false);
+  const [coords, setCoords] = useState(undefined);
+  const [current, setCurrent] = useState({});
+  const [daily, setDaily] = useState([]);
+
+  const fetchCoordinates = async (city) => {
+    const response = await fetch(`${LOCATION_URL}${city}&appid=${API_KEY}`);
+    const { coord } = await response.json();
+    console.log(coord);
+    setCoords(coord);
+  };
+
+  const fetchWeather = async (coord) => {
+    const response = await fetch(
+      `${WEATHER_URL}lat=${coord.lat}&lon=${coord.lon}&units=metric&exclude=minutely,hourly&appid=${API_KEY}`
+    );
+    const data = await response.json();
+    console.log(data);
+    const newWeather = {
+      temp: data.current.temp,
+      icon: data.current.weather[0].icon,
+    };
+    setCurrent(newWeather);
+    setDaily(data.daily);
+    console.log(newWeather);
+    console.log(data.daily);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    fetchCoordinates(location);
+  }, [location]);
+
+  useEffect(() => {
+    if (coords) fetchWeather(coords);
+  }, [coords]);
+
+  return loading ? (
+    <div>Loading...</div>
+  ) : (
     <div>
-      <h2>Min: {temp_min}°C</h2>
-      <h2>Max: {temp_max}°C</h2>
+      <div>
+        <h1>Weather for {location}:</h1>
+      </div>
+      <h2>Temp: {current.temp}°C</h2>
     </div>
   );
 };
